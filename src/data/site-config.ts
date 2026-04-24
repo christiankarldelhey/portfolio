3f293e77b9a8a503bc5a5960ee077f0f660e23ce
Christian <caascaas@gmail.com>
2026-04-24 17:03:41 +0200

    Configure GitHub Pages deployment

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hasen.png';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
+    website: 'https://christiankarldelhey.github.io/portfolio/',
     avatar: {
         src: avatar,
         alt: 'Christian Karl Delhey'
